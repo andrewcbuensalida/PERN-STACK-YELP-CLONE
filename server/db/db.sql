70cbd3b6512118783273eca6bf5caf5cd71cd474
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-28 23:54:27 -0700

    fixed rating stars

--- server/db/db.sql
+++ server/db/db.sql
@@ -13,7 +13,7 @@ from restaurants
     left join(
         select restaurant_id,
             count(*),
-            TRUNC(AVG(rating, 1)) as average_rating
+            TRUNC(AVG(rating), 1) as average_rating
         from reviews
         group by restaurant_id
     ) reviews on restaurants.id = reviews.restaurant_id;
--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,7 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-	user: process.env.PGUSER,
-	host: process.env.PGHOST,
-	password: process.env.PGPASSWORD,
-	database: process.env.PGDATABASE,
-	port: process.env.PGPORT,
-});
+const pool = new Pool();
 module.exports = {
 	query: (text, params) => pool.query(text, params),
 };
