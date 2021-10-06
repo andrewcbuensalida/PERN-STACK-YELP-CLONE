6d8eb519d2bbb6be54000d07b1ddb7c42657363b
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-10-06 11:38:55 -0700

    working

--- client/src/components/DoctorList.js
+++ client/src/components/DoctorList.js
@@ -14,16 +14,14 @@ const DoctorList = () => {
 
 	useEffect(() => {
 		fetchDoctors();
-	}, [doctors]);
+	}, []);
 
 	const getMoreData = () => {
 		if (current.length === doctors.length) {
 			setHasMore(false);
 			return;
 		}
-
 		setCurrent(current.concat(doctors.slice(count.prev + 10, count.next + 10)));
-
 		setCount((prevState) => ({
 			prev: prevState.prev + 10,
 			next: prevState.next + 10,
@@ -34,51 +32,48 @@ const DoctorList = () => {
 		try {
 			const response = await DoctorFinder.get(`/name/${offset.current}/ASC`);
 			setDoctors((prevDoctors) => {
+				setCurrent(() =>
+					response.data.data.doctors.slice(count.prev, count.next)
+				);
 				return [...prevDoctors, ...response.data.data.doctors];
 			});
-			setCurrent(doctors.slice(count.prev, count.next));
 		} catch (err) {}
 		// } finally {
 		// 	setCurrent(doctors.slice(count.prev, count.next));
 		// }
 	};
 
-	console.log("This is current");
-	console.log(current);
 	return (
 		<div className="list-group">
-			<table className="table table-hover table-dark">
-				<thead>
-					<tr className="bg-primary">
-						<th scope="col">Doctor</th>
-						<th scope="col">Company</th>
-						<th scope="col">Price Range</th>
-						<th scope="col">Ratings</th>
-						<th scope="col">Edit</th>
-						<th scope="col">Delete</th>
-					</tr>
-				</thead>
-				<tbody>
-					{" "}
-					<InfiniteScroll
-						dataLength={current.length}
-						next={getMoreData}
-						hasMore={hasMore}
-						loader={<h4>Fetching...</h4>}
-					>
-						<div>
-							{current &&
-								current.map((doctor) => (
-									<DoctorInList
-										key={doctor.id}
-										doctor={doctor}
-										setDoctors={setDoctors}
-									/>
-								))}
-						</div>
-					</InfiniteScroll>
-				</tbody>
-			</table>
+			<InfiniteScroll
+				dataLength={current.length}
+				next={getMoreData}
+				hasMore={hasMore}
+				loader={<h4>Fetching...</h4>}
+			>
+				<table className="table table-hover table-dark">
+					<thead>
+						<tr className="bg-primary">
+							<th scope="col">Doctor</th>
+							<th scope="col">Company</th>
+							<th scope="col">Price Range</th>
+							<th scope="col">Ratings</th>
+							<th scope="col">Edit</th>
+							<th scope="col">Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						{" "}
+						{current.map((doctor) => (
+							<DoctorInList
+								key={doctor.id}
+								doctor={doctor}
+								setDoctors={setDoctors}
+							/>
+						))}
+					</tbody>
+				</table>
+			</InfiniteScroll>
 		</div>
 	);
 };
