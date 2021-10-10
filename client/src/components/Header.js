81d5aabbcd6a96e81879b01f3669274275935363
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-10-10 14:39:51 -0700

    finished

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
 	return (
 		<div>
-			<h1 className="font-weight-light display-1 text-center">Doctor db</h1>
+			<h1 className="font-weight-light display-1 text-center">
+				Doctor Database
+			</h1>
 		</div>
 	);
 };
