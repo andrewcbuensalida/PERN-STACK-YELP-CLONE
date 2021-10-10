ec6f4862f82663cdfed36ad299a9ec3efac43e2a
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-10-10 10:17:36 -0700

    k

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -8,5 +8,5 @@ export NVM_DIR="$HOME/.nvm"
 #Stopping existing node servers
 echo "Stopping any existing node server for real this time, not really"
 # pkill node
-/home/ubuntu/.nvm/versions/node/v16.11.0/bin/pm2 stop doctordb_node > app.out.log 2> app.err.log < /dev/null &
-/home/ubuntu/.nvm/versions/node/v16.11.0/bin/pm2 delete doctordb_node > app.out.log 2> app.err.log < /dev/null &
+# /home/ubuntu/.nvm/versions/node/v16.11.0/bin/pm2 stop doctordb_node > app.out.log 2> app.err.log < /dev/null &
+# /home/ubuntu/.nvm/versions/node/v16.11.0/bin/pm2 delete doctordb_node > app.out.log 2> app.err.log < /dev/null &
