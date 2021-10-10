944455537046a632ae081d89c60354cd2d838e13
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-10-09 21:11:27 -0700

    jkh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# export NVM_DIR="$HOME/.nvm"	
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #Stopping existing node servers
 echo "Stopping any existing node server for real this time, not really"
