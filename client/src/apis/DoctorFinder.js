86b2db4faf4f0482986008bc7a65e7bf85c17589
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-10-10 23:06:26 -0700

    h

--- client/src/apis/DoctorFinder.js
+++ client/src/apis/DoctorFinder.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
 	process.env.NODE_ENV === "production"
-		? "/doctordb/api/v1/doctors"
+		? "http://localhost/doctordb/api/v1/doctors"
 		: "18.144.56.242/doctordb/api/v1/doctors";
 
 export default axios.create({
